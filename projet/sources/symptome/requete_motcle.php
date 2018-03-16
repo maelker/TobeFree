22240ddeded396a7f0bef101ea6b544115cfd374
Mael Kerloch <mael.kerloch@gmail.com>
2018-03-16 17:35:42 +0100

    Add files via upload
    MAJ fichiers symptomes

--- projet/sources/symptome/requete_motcle.php
+++ projet/sources/symptome/requete_motcle.php
@@ -12,67 +12,41 @@ $tpl = new Smarty();
 
 $tpl->compile_dir='../../templates_c/';
 
-
+$mot_cle_requete=array();
+$mot_cle_affiche=array();
+$tableau_mot=array();
+//$tpl->display("symptome.html");
 
 if (isset($_POST['keyword'])){
 
-	$mot_cle_requete=$_POST['keyword'];
-	$_POST['mot_cle']=$mot_cle_requete;
-
-	//$mot_cle_affiche=$_POST['mot_cle'];
-
-
-	$query1 = $connexion->prepare("SELECT DISTINCT symptome.desc FROM symptome,keywords,keySympt WHERE keywords.idK=keySympt.idK AND keySympt.idS=symptome.idS AND keywords.name='$mot_cle_requete'");
-
-	$query1->execute();
-
-
-	$list_symptome = array();
-
+	$liste_mot=$_POST['keyword'];
+	$tableau_mot=explode(",",$liste_mot);
+	$tpl->assign('tableau_mot',$tableau_mot);
 	$i = 0;
+	for ($j=0;$j<count($tableau_mot);$j++)
+	{
+		$mot_cle_requete[$j]=$tableau_mot[$j];
+		$mot_cle_affiche[$j]=$mot_cle_requete[$j];
+		$query1 = $connexion->prepare("SELECT DISTINCT symptome.desc FROM symptome,keywords,keySympt WHERE keywords.idK=keySympt.idK AND keySympt.idS=symptome.idS AND keywords.name='$mot_cle_requete[$j]'");
 
-	while($data = $query1->fetch()){
+		$query1->execute();
 
-	    $list_symptome[$i]['desc'] = $data['desc'];
+		
 
-	    $i++;
+		while($data = $query1->fetch()){
 
-	}
-	$query1->closeCursor();
-	$tpl->assign('list_symptome', $list_symptome);
-	$tpl->assign('mot_cle_affiche',$mot_cle_affiche);
-
-}
-
-if (isset($_POST['keyword2'])){
-
-		$mot_cle_requete2=$_POST['keyword2'];
-		$mot_cle_affiche2=$_POST['keyword2'];
-
-	//$mot_cle_affiche2; // ou $_POST['keyword2'] si jamais on le crée
-		$query2 = $connexion->prepare("SELECT DISTINCT symptome.desc FROM symptome,keywords,keySympt WHERE keywords.idK=keySympt.idK AND keySympt.idS=symptome.idS AND keywords.name='$mot_cle_requete2'");
-
-		$query2->execute();
-
-
-		$list_symptome2 = array();
-
-		$i = 0;
-
-		while($data2 = $query2->fetch()){
-
-		    $list_symptome2[$i]['desc'] = $data2['desc'];
+		    $list_symptome[$i]['desc'] = $data['desc'];
 
 		    $i++;
 
 		}
-		$query2->closeCursor();
-		$tpl->assign('list_symptome2', $list_symptome2);
-
-		$tpl->assign('mot_cle_affiche2',$mot_cle_affiche2);
+		$query1->closeCursor();
+		$tpl->assign('list_symptome', $list_symptome);
+		$tpl->assign('mot_cle_affiche',$mot_cle_affiche);
+		//$tpl->display("resultat_symptome.html");
 	}
-
-
+//$tpl->display("resultat_symptome.html");
+}
 
 $tpl->display("symptome.html");
 
