7b1ccb75a73edadafc22e56033020f8f7be82e40
Thibault C <thibault.csnl@gmail.com>
2018-03-16 17:19:16 +0100

    connexion - deconnexion

---
+++ projet/sources/login/deconnexion.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+// Suppression des variables de session et de la session
+$_SESSION = array();
+
+session_destroy();
+
+// Suppression des cookies de connexion automatique
+
+setcookie('login', '');
+
+//setcookie('pass_hache', '');
+
+echo "Vous êtes deconnecté";
+
+
+
+?>
